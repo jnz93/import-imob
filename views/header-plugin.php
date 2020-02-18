816307e8e3642abf4308326f551ee35e971e0fb6
joanezandrades <box@unitycode.tech>
2020-02-18 11:59:32 -0300

    Add header and footer templates

---
+++ views/header-plugin.php
@@ -0,0 +1,7 @@
+<main class="container-fluid row justify-content-center" role="main">
+    <section id="content-home" class="row justify-content-center col-lg-10 col-xl-10">
+        <div class="row justify-content-center col-lg-10 col-xl-10">
+            <header id="header-main" class="col-lg-10 col-xl-10">
+                <h1 class=""><?php _e('Import Imob - PLUGIN', 'TEXT_DOMAIN'); ?></h1>
+                <p class=""><?php _e('Importação de imóveis selecionados na plataforma Ingaia Imob.', 'TEXT_DOMAIN'); ?></p>
+            </header>
---
+++ views/setup-plugin.php
@@ -0,0 +1,7 @@
+<?php include_once(plugin_dir_path(__FILE__) . '/header-plugin.php'); ?>
+
+    <?php #ImportImob::publish_properties(); ?>
+    <?php #ImportImob::xml_to_arr(); ?>
+    <?php ImportImob::setup_options(); ?>
+
+<?php include_once(plugin_dir_path(__FILE__) . '/footer-plugin.php');?>
