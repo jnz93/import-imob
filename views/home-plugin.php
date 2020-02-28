e92a069a0ed5900f2d777f8eef2ac084e12e4019
joanezandrades <box@unitycode.tech>
2020-02-28 15:51:37 -0300

    [FIX] Homepage final

--- views/home-plugin.php
+++ views/home-plugin.php
@@ -1,7 +1,5 @@
 <?php include_once(plugin_dir_path(__FILE__) . '/header-plugin.php'); ?>
 
-    <?php #ImportImob::publish_properties(); ?>
-    <?php #ImportImob::xml_to_arr(); ?>
-    <?php #ImportImob::setup_options(); ?>
-
+    <?php ImportImob::log_load_properties(); ?>
+    
 <?php include_once(plugin_dir_path(__FILE__) . '/footer-plugin.php');?>
